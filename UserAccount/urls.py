a0e795970141b743efb92e6af95c5dfbfdb8409a
mitanshi <mitanshi1995.mr@gmail.com>
2017-06-17 18:13:40 +0530

    Changes in Login

--- UserAccount/urls.py
+++ UserAccount/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import include,url
-from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import login, password_reset,password_reset_done,password_reset_confirm,password_reset_complete
 from . import views
 urlpatterns = [
-    url(r'^login/', views.home),
-    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
-    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^login/', login, {'template_name': 'UserAccount/login.html'}),
+    url(r'^password_reset/$', password_reset, {'template_name': 'UserAccount/password_reset_form.html'}),
+    url(r'^password_reset/done/$',password_reset_done, name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.password_reset_confirm, name='password_reset_confirm'),
-    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+        password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', password_reset_complete, name='password_reset_complete'),
 ]
