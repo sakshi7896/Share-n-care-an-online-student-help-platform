cee8742ec8528e3ea0e93d71768abbc8a627868a
Sonali Bansal <sonalibansal.1901.com>
2017-06-27 15:48:38 +0530

    User registration form and login updated

--- UserAccount/views.py
+++ UserAccount/views.py
@@ -3,34 +3,60 @@ from UserAccount.form import *
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 
-# Create your views here.
-def home(request):
-    return render(request, 'UserAccount/login.html')
+from django.db import models
 
-def index(request):
-    return HttpResponse("<h2> HEY! </h2>")
+from UserAccount.form import SignUpForm
+from UserAccount.models import Profile
+from django.contrib.auth.models import User
 
-def register(request):
-    if request.method == 'POST':
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            user = User.objects.create_user(username=form.cleaned_data['username'],password=form.cleaned_data['password'],email=form.cleaned_data['email'],mobile=form.cleaned_data['mobile'],year=form.cleaned_data['year'],branch=form.cleaned_data['branch'],course=form.cleaned_data['course'])
-            return HttpResponseRedirect('/')
-    form = RegistrationForm()
-    #variables = RequestContext(request, {'form': form})
-    return render(request,'UserAccount/register.html',{'form': form})
 
 
 def login_user(request):
     if request.method == "POST":
         mail = request.POST['username']
         password = request.POST['password']
+        #return HttpResponse(password)
+
         user = authenticate(username=mail, password=password)
         if user is not None:
+            #return HttpResponse("user is there")
             if user.is_active:
-                return HttpResponse("<hello>")
+                return HttpResponse("hello")
             else:
-                return HttpResponse("<Bye>")
+                return HttpResponse("Bye")
         else:
-            return HttpResponse("No user")
-    return "Error"
+            return render(request, 'UserAccount/login.html',{'error_message':"Invlid user details"})
+    return render(request, 'UserAccount/login.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST or None)
+        if form.is_valid():
+            username = request.POST.get('username')
+            email =request.POST.get('email')
+            password = request.POST.get('password1')
+            phone_number=request.POST.get('phone_number')
+            year=request.POST.get('year')
+            branch=request.POST.get('branch')
+            course=request.POST.get('course')
+            
+            
+            p1=User(username=username,email=email,password=password)
+            p1.set_password(password)
+            p1.save()
+            newUser=Profile(user=p1,phone_number=phone_number,year=year,branch=branch,course=course)
+            newUser.save()
+
+
+            
+            return HttpResponse('<h2>registration successful</h2>')
+        else:
+            form = SignUpForm()
+            context={
+            'form':form,
+            'error_message':"Invalid User details"
+            }
+            return render(request, 'UserAccount/register.html',context)
+
+    form = SignUpForm()
+    return render(request, 'UserAccount/register.html',{'form': form})
