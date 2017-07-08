d9b887ee9ee3f4339a9c25968c5fa0e59480cc5e
Sonali Bansal <sonalibansal.1901.com>
2017-07-08 13:56:15 +0530

    deploying project on heroku

--- ShareAndCare/settings.py
+++ ShareAndCare/settings.py
@@ -117,10 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
 LOGIN_REDIRECT_URL ='/UserAccount/login.html'
 
+STATIC_URL = '/static/'
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
@@ -128,10 +129,11 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join('static'),
+    os.path.join('layout/static'),
 )
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
