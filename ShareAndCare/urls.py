98723b58f3f79c5b5ecf8b746309e8dd75ae9577
mitanshi <mitanshi1995.mr@gmail.com>
2017-07-09 10:15:18 +0530

    Fixed the demo page and other stuff

--- ShareAndCare/urls.py
+++ ShareAndCare/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf.urls import url,include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from UserAccount import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^UserAccount/', include('UserAccount.urls')),
+    url(r'^$',views.index,name='index'),
 
 ]
--- UserAccount/urls.py
+++ UserAccount/urls.py
@@ -14,7 +14,6 @@ urlpatterns = [
     url(r'^search/',views.search_view, name='search_view'),
     url(r'^search_book/',views.search_book, name='search_book'),
     url(r'^book_form/$', views.new_book_post, name='book_form'),
-    url(r'^demo/$',views.demo, name='demo'),
 
 
 
