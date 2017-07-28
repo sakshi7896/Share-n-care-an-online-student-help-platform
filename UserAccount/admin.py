f43e1b4bcd487fde8b2e48aa7e8a95b9903f7dcd
sakshi7896 <sakshi7896@gmail.com>
2017-07-28 16:17:36 +0530

    Added 10 books

--- UserAccount/admin.py
+++ UserAccount/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Profile, Book
+from .models import Profile, Book, Counselling
 from .models import Profile
 from django.db import models
 admin.site.register(Profile)
 admin.site.register(Book)
+admin.site.register(Counselling)
